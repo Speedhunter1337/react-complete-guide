4cd56636df36807ca397f2cdbfe1cd323adeaf54
Speedhunter1337 <101300138+Speedhunter1337@users.noreply.github.com>
2022-06-18 12:37:30 -0700

    My Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,38 @@
 import ExpenseItem from "./ExpenseItem";
 
 const expenses = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
+	{
+		id: "e1",
+		title: "Toilet Paper",
+		amount: 94.12,
+		date: new Date(2020, 7, 14),
+	},
+	{ id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+	{
+		id: "e3",
+		title: "Car Insurance",
+		amount: 294.67,
+		date: new Date(2021, 2, 28),
+	},
+	{
+		id: "e4",
+		title: "New Desk (Wooden)",
+		amount: 450,
+		"emmet.includeLanguages": {},
+		date: new Date(2021, 5, 12),
+	},
 ];
 
-const App = (props) => {
-  const renderExpenses = expenses.map((expense) => (
-    <ExpenseItem key={expense.id} expense={expense} />
-  ));
+const App = props => {
+	const renderExpenses = expenses.map(expense => (
+		<ExpenseItem key={expense.id} expense={expense} />
+	));
 
-  return <div>{renderExpenses}</div>;
+	return (
+		<div>
+			<div>{renderExpenses}</div>
+		</div>
+	);
 };
 
 export default App;
