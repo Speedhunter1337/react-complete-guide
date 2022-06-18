d509dc5e0d3b30e6c43826860ada3fdc12230b0c
Speedhunter1337 <john.dh.park@icloud.com>
2022-06-18 13:38:04 -0700

    Merge Pull

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,37 @@
-import ExpenseItem from "./ExpenseItem";
 import Expenses from "./Expenses";
 
 const expenses = [
-	{
-		id: "e1",
-		title: "Toilet Paper",
-		amount: 94.12,
-		date: new Date(2020, 7, 14),
-	},
-	{ id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-	{
-		id: "e3",
-		title: "Car Insurance",
-		amount: 294.67,
-		date: new Date(2021, 2, 28),
-	},
-	{
-		id: "e4",
-		title: "New Desk (Wooden)",
-		amount: 450,
-		"emmet.includeLanguages": {},
-		date: new Date(2021, 5, 12),
-	},
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    "emmet.includeLanguages": {},
+    date: new Date(2021, 5, 12),
+  },
 ];
 
 const App = (props) => {
-	return (
-		<div>
-			<div>
-				<Expenses expenses={expenses} />
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <h2>Let's get started</h2>
+      <div>
+        <Expenses expenses={expenses} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
